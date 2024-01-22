7142a76b3457b60456273866519d6a2305c32ffe
oliullah05 <mdolihasan@gmail.com>
2024-01-22 21:21:18 +0600

    Nav bar done

--- src/Pages/Shared/Header/Header.jsx
+++ src/Pages/Shared/Header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from '../../../assets/logo.png'
 import moment from 'moment';
-import { Button, Container } from 'react-bootstrap';
+import { Button, Container, Nav, NavDropdown, Navbar } from 'react-bootstrap';
 import Marquee from "react-fast-marquee";
 
 const Header = () => {
@@ -15,9 +15,29 @@ const Header = () => {
             <div className='d-flex mb-5'>
                 <Button variant="danger">Danger</Button>
                 <Marquee speed={100}>
-                        The quick brown fox jumps over the lazy  dog The quick brown fox jumps over the lazy  dog The quick brown fox jumps over the lazy  dog
+                    The quick brown fox jumps over the lazy  dog The quick brown fox jumps over the lazy  dog The quick brown fox jumps over the lazy  dog
                 </Marquee>
             </div>
+
+            <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mx-auto">
+            <Nav.Link href="#features">Home</Nav.Link>
+            <Nav.Link href="#pricing">About</Nav.Link>
+            <Nav.Link href="#pricing">Career</Nav.Link>
+           
+          </Nav>
+          <Nav >
+            <Nav.Link className='my-auto' href="#deets">Profile</Nav.Link>
+            <Nav.Link eventKey={2} href="#memes">
+             <Button variant='secondary'>Log in</Button>
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
         </Container>
     );
 };
